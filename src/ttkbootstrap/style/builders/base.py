3c0aa7bc6894d5bec1f08cbdcc870998ca7b49d5
Israel Dryer <israel.dryer@gmail.com>
2025-06-30 17:49:44 -0400

    add base style builder

---
+++ src/ttkbootstrap/style/builders/base.py
@@ -0,0 +1,44 @@
+from ..style import Style
+from ..theme import ColorTheme
+
+
+class StyleBuilderBase:
+
+    def __init__(self, target_style: str, surface: str = None, **options):
+        self._target = target_style
+        self._surface = surface or "base"
+        self._theme = ColorTheme.instance()
+        self._style = Style()
+        self.options = options or dict()
+
+    @property
+    def theme(self):
+        return self._theme
+
+    def surface(self, value: str = None):
+        """Get or set the surface type"""
+        if value is None:
+            return self._surface
+        else:
+            self._surface = value
+            return self
+
+    def resolve_name(self):
+        """Return the resolved ttk style name"""
+        return ",".join(
+            [str(x) for x in self.options.values()] + [self.surface()] + [self.theme.name, self._target]
+        )
+
+    def exists(self):
+        return self._style.style_exists(self.resolve_name())
+
+    def build(self):
+        """Build and return style name"""
+        name = self.resolve_name()
+        if not self.exists():
+            self.register_style()
+        return name
+
+    def register_style(self):
+        """Register the style (to be implemented by subclass"""
+        raise NotImplemented("Must be implemented by subclass")
