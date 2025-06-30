8ae037031eda9428d32f05e1eaa528a699ec6dba
Israel Dryer <israel.dryer@gmail.com>
2025-06-29 20:59:08 -0400

    add initial geometry mixin

---
+++ src/ttkbootstrap/core/mixins/geometry.py
@@ -0,0 +1,18 @@
+# geometry managers
+from tkinter import Misc
+from tkinter import Widget
+from typing import TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from ttkbootstrap.core.widget import BaseWidget
+
+
+class GeometryMixin:
+    widget: Union["BaseWidget", Widget]
+
+    def pack(self, option=None, **kwargs):
+        if option is None:
+            self.widget.pack(**kwargs)
+            return self
+        else:
+            return self.widget.pack(option)
