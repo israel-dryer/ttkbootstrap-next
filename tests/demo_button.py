1a3daa5db219494147b1d458669739b1a3e2354e
Israel Dryer <israel.dryer@gmail.com>
2025-07-01 16:57:28 -0400

    update button demo

--- tests/demo_button.py
+++ tests/demo_button.py
@@ -1,8 +1,25 @@
+from tkinter import ttk
+
 from ttkbootstrap.core import App
 from ttkbootstrap.widgets.button import Button
+from ttkbootstrap.style.style import Style
 
-app = App("Button Demo")
+app = App("Button Demo", theme="light")
 
-btn = Button(app, "Push me").pack().on_click(lambda: print("Hello world"))
+style = Style()
+
+Button(app, "Primary", color="primary").pack(padx=16, pady=16)
+Button(app, "Secondary", color="secondary").pack(padx=16, pady=16)
+Button(app, "Success", color="success").pack(padx=16, pady=16)
+Button(app, "Warning", color="warning").pack(padx=16, pady=16)
+Button(app, "Danger", color="danger").pack(padx=16, pady=16)
+Button(app, "Info", color="info").pack(padx=16, pady=16)
+Button(app, "Light", color="light").pack(padx=16, pady=16)
+Button(app, "Dark", color="dark").pack(padx=16, pady=16)
+
+
+def get_style():
+    print(btn.configure('style'))
+
 
 app.run()
