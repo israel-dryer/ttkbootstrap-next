a0fbc365fd2e1beabb86667c26fd01789ac5d6e1
Israel Dryer <israel.dryer@gmail.com>
2025-07-02 06:18:46 -0400

    update button demo

--- tests/demo_button.py
+++ tests/demo_button.py
@@ -8,18 +8,19 @@ app = App("Button Demo", theme="light")
 
 style = Style()
 
+b1 = Button(app, "Primary", color="primary", icon="house").pack(padx=16, pady=16).disable()
+b2 = Button(app, "Primary", color="primary", icon="house").pack(padx=16, pady=16)
 Button(app, "Primary", color="primary").pack(padx=16, pady=16)
-Button(app, "Secondary", color="secondary", size="lg").pack(padx=16, pady=16)
-Button(app, "Success", color="success", size="sm").pack(padx=16, pady=16)
+Button(app, "Secondary", color="secondary", size="lg", icon="house").pack(padx=16, pady=16)
+Button(app, "Success", color="success", size="sm", icon="chevron-right", compound="right").pack(padx=16, pady=16)
 Button(app, "Warning", color="warning").pack(padx=16, pady=16)
 Button(app, "Danger", color="danger").pack(padx=16, pady=16)
 Button(app, "Info", color="info").pack(padx=16, pady=16)
-Button(app, "Light", color="light").pack(padx=16, pady=16)
-Button(app, "Dark", color="dark").pack(padx=16, pady=16)
+lt = Button(app, "Light", color="light").pack(padx=16, pady=16)
+tb = Button(app, "Dark", color="dark").pack(padx=16, pady=16)
 
-
-def get_style():
-    print(btn.configure('style'))
+tb.on_click(lambda: app.theme.use('dark'))
+lt.on_click(lambda: b2.color('info'))
 
 
 app.run()
