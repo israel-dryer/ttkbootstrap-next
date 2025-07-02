a57e9050f93489cf3c9d8eb2c6c37559d50e96b0
Israel Dryer <israel.dryer@gmail.com>
2025-07-02 14:55:27 -0400

    update button demo

--- tests/demo_button.py
+++ tests/demo_button.py
@@ -1,12 +1,7 @@
-from tkinter import ttk
-
 from ttkbootstrap.core import App
 from ttkbootstrap.widgets.button import Button
-from ttkbootstrap.style.style import Style
 
-app = App("Button Demo", theme="light")
-
-style = Style()
+app = App("Button Demo", surface="accent")
 
 b1 = Button(app, "Primary", color="primary", icon="house").pack(padx=16, pady=16).disable()
 b2 = Button(app, "Primary", color="primary", icon="house").pack(padx=16, pady=16)
@@ -15,12 +10,12 @@ Button(app, "Secondary", color="secondary", size="lg", icon="house").pack(padx=1
 Button(app, "Success", color="success", size="sm", icon="chevron-right", compound="right").pack(padx=16, pady=16)
 Button(app, "Warning", color="warning").pack(padx=16, pady=16)
 Button(app, "Danger", color="danger").pack(padx=16, pady=16)
-Button(app, "Info", color="info").pack(padx=16, pady=16)
+ac = Button(app, "Accent Bg", color="info").pack(padx=16, pady=16)
 lt = Button(app, "Light", color="light").pack(padx=16, pady=16)
 tb = Button(app, "Dark", color="dark").pack(padx=16, pady=16)
 
+ac.on_click(lambda: app.surface('muted'))
 tb.on_click(lambda: app.theme.use('dark'))
-lt.on_click(lambda: b2.color('info'))
-
+lt.on_click(lambda: app.theme.use('light'))
 
 app.run()
