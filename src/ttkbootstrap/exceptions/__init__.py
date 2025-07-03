6fe97802131bfee472fe14b0537397de09a22ebb
Israel Dryer <israel.dryer@gmail.com>
2025-07-03 03:08:36 -0400

    add invalid token exception

--- src/ttkbootstrap/exceptions/__init__.py
+++ src/ttkbootstrap/exceptions/__init__.py
@@ -14,3 +14,21 @@ class InvalidThemeError(Exception):
         else:
             full_message = f"Invalid theme: {message}"
         super().__init__(full_message)
+
+
+class InvalidTokenError(Exception):
+    """Exception raised when an invalid style token is provided
+
+    Attributes:
+        message: Explanation of the error.
+        token: The name of the color token provided
+    """
+
+    def __init__(self, message: str, token: str = None):
+        self.message = message
+        self.token = token
+        if token:
+            full_message = f"Invalid style token '{token}': {message}"
+        else:
+            full_message = f"Invalid style token: {message}"
+        super().__init__(full_message)
