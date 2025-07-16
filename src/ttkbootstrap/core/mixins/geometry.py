07f333abae3d06052c378293599fc606828452ba
Israel Dryer <israel.dryer@gmail.com>
2025-07-15 23:39:18 -0400

    implement basic grid

--- src/ttkbootstrap/core/mixins/geometry.py
+++ src/ttkbootstrap/core/mixins/geometry.py
@@ -20,3 +20,17 @@ class GeometryMixin:
     def pack_forget(self):
         self.widget.pack_forget()
         return self
+
+    def grid(self, option=None, **kwargs):
+        if option is None:
+            self.widget.grid(**kwargs)
+            return self
+        else:
+            return self.widget.grid(option)
+
+    def grid_column_configure(self, index, *, option=None, **kwargs):
+        if option is None:
+            self.widget.columnconfigure(index, **kwargs)
+            return self
+        else:
+            return self.widget.columnconfigure(index, option)
