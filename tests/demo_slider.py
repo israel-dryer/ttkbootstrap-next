d63e9aa51d183fce558054ad24c1333cc3a4fa69
Israel Dryer <israel.dryer@gmail.com>
2025-07-16 16:35:27 -0400

    implement scale style

--- tests/demo_slider.py
+++ tests/demo_slider.py
@@ -1,9 +1,10 @@
 from ttkbootstrap.core import App
-from ttkbootstrap.widgets import Slider
+from ttkbootstrap.widgets import Scale
 
 app = App(title="Scale Demo")
 
-scale = Slider(app, value=25, on_change=lambda x: print(x)).pack(padx=16, pady=16, fill='x')
-
+Scale(app, color="success", value=40, orient="vertical").pack(padx=16, pady=16, fill="y", expand=True)
 
 app.run()
+
+
