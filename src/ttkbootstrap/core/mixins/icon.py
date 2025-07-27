5fe37d76ad1130538d75a54c04f75c65ba613842
Israel Dryer <israel.dryer@gmail.com>
2025-07-26 22:17:46 -0400

    more efficient handling of icon state changes

--- src/ttkbootstrap/core/mixins/icon.py
+++ src/ttkbootstrap/core/mixins/icon.py
@@ -13,35 +13,43 @@ class IconMixin:
     def __init__(self):
         self._selected_state_icon = False
         self._stateful_icons_bound = False
+        self._current_icon_image = None
         self._update_icon_assets()
+
+    def _set_image_if_needed(self, image):
+        """Set the image only if it differs from the current."""
+        if self._current_icon_image != image:
+            self._current_icon_image = image
+            self.configure(image=image)
 
     def icon(self, value=None):
         """Get or set the widget icon"""
         if value is None:
             return self._icon
-        self._icon = value
-        self._update_icon_assets()
+        if self._icon != value:
+            self._icon = value
+            self._current_icon_image = None  # force image reset
+            self._update_icon_assets()
 
         # Re-apply current state image
         icons = self._style_builder.stateful_icons
-
         if self._selected_state_icon and 'selected' in icons:
-            self.configure(image=icons['selected'])
+            self._set_image_if_needed(icons['selected'])
         elif self._is_icon_disabled():
-            self.configure(image=icons['disabled'])
+            self._set_image_if_needed(icons['disabled'])
         elif self.has_focus():
-            self.configure(image=icons['focus'])
+            self._set_image_if_needed(icons['focus'])
         else:
-            self.configure(image=icons['normal'])
+            self._set_image_if_needed(icons['normal'])
 
-        # update icon position if adding icon after button is initialized
         if hasattr(self, 'icon_position') and hasattr(self, '_icon_position'):
             self.icon_position(self._icon_position)
         self.process_idle_tasks()
         return self
 
     def _update_icon_assets(self):
-        if not self._icon: return
+        if not self._icon:
+            return
         self._style_builder.build_icon_assets(self._icon)
         if not self._stateful_icons_bound:
             self._bind_stateful_icons()
@@ -52,43 +60,40 @@ class IconMixin:
         if self._stateful_icons_bound:
             return
         icons = self._style_builder.stateful_icons
-        self.configure(image=icons['normal'])
+        self._set_image_if_needed(icons['normal'])
 
         def on_enter(_):
-            if self._is_icon_disabled() or self._selected_state_icon: return
-            if self._selected_state_icon: return
-            self.configure(image=icons['hover'])
+            if self._is_icon_disabled() or self._selected_state_icon:
+                return
+            self._set_image_if_needed(icons.get('hover', icons['normal']))
 
         def on_leave(_):
             if self._is_icon_disabled() or self._selected_state_icon:
                 return
-            elif self.has_focus():
-                self.configure(image=icons['focus'])
+            if self.has_focus():
+                self._set_image_if_needed(icons.get('focus', icons['normal']))
             else:
-                self.configure(image=icons['normal'])
+                self._set_image_if_needed(icons['normal'])
 
         def on_press(_):
-            if self._is_icon_disabled():
-                return
-            if 'pressed' in icons:
-                self.configure(image=icons['pressed'])
+            if not self._is_icon_disabled() and 'pressed' in icons:
+                self._set_image_if_needed(icons['pressed'])
 
         def on_focus_in(_):
-            if self._is_icon_disabled():
-                self.configure(image=icons['focus'])
+            if not self._is_icon_disabled():
+                self._set_image_if_needed(icons.get('focus', icons['normal']))
 
         def on_focus_out(_):
-            if self._is_icon_disabled(): return
-            self.configure(image=icons['normal'])
+            if not self._is_icon_disabled():
+                self._set_image_if_needed(icons['normal'])
 
         def on_selected(_):
             self._selected_state_icon = True
-            if 'selected' in icons:
-                self.configure(image=icons['selected'])
+            self._set_image_if_needed(icons.get('selected', icons['normal']))
 
         def on_deselected(_):
             self._selected_state_icon = False
-            self.configure(image=icons['normal'])
+            self._set_image_if_needed(icons['normal'])
 
         self.bind('enter', on_enter)
         self.bind('leave', on_leave)
@@ -100,16 +105,13 @@ class IconMixin:
 
         self._stateful_icons_bound = True
 
-        # set disabled state
         if self._is_icon_disabled():
-            self.configure(image=icons['disabled'])
+            self._set_image_if_needed(icons['disabled'])
 
     def _toggle_disable_icon(self, disable=True):
         icons = self._style_builder.stateful_icons
-        if disable:
-            self.configure(image=icons['disabled'])
-        else:
-            self.configure(image=icons['normal'])
+        image = icons['disabled'] if disable else icons['normal']
+        self._set_image_if_needed(image)
         self.process_idle_tasks()
 
     def _is_icon_disabled(self):
