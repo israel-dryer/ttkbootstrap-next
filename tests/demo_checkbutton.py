15605d3a35dfb83d7e618be27f2ebc17a289672e
Israel Dryer <israel.dryer@gmail.com>
2025-08-13 01:48:08 -0400

    update demo with new layout

--- tests/demo_checkbutton.py
+++ tests/demo_checkbutton.py
@@ -1,29 +1,13 @@
-from ttkbootstrap.core import App
+from ttkbootstrap import App
+from ttkbootstrap.layouts.flexgrid import FlexGrid
 from ttkbootstrap.widgets import CheckButton
 
-app = App(title="Demo Checkbutton", theme="dark")
-
-cb1 = CheckButton(app, text="Unchecked", color="success", value=0).pack()
-CheckButton(app, text="Unchecked", color="success", value=0).pack()
-CheckButton(app, text="Unchecked", value=0).pack()
-cb2 = CheckButton(app, text="Indeterminate", value=-1, on_toggle=lambda: print('Clicked')).pack().disable()
-cb3 = CheckButton(app, text="Checked", value=1, on_change=lambda x: print(x)).pack()
-print('one', cb1.state())
-print('two', cb2.state())
-print('three', cb3.state())
+with App("Demo Checkbutton", theme="dark") as app:
+    with FlexGrid(gap=8, padding=8, sticky_items="ew"):
+        CheckButton(text="Unchecked", color="success", value=0)
+        CheckButton(text="Unchecked", color="success", value=0)
+        CheckButton(text="Unchecked", value=0)
+        CheckButton(text="Indeterminate", value=-1, on_toggle=lambda: print('Clicked')).disable()
+        CheckButton(text="Checked", value=1, on_change=lambda x: print(x))
 
 app.run()
-
-# import tkinter as tk
-# from tkinter import ttk
-#
-# root = tk.Tk()
-# style = ttk.Style()
-# style.theme_use('clam')
-#
-# print(style.layout('TCheckbutton'))
-# print(style.map('TCheckbutton'))
-#
-# ttk.Checkbutton(root).pack()
-#
-# root.mainloop()
