d7427bf5608b2b2de504e7145f3e4589c72def84
Israel Dryer <israel.dryer@gmail.com>
2025-08-31 02:18:34 -0400

    update style builders

--- src/ttkbootstrap/style/builders/scale.py
+++ src/ttkbootstrap/style/builders/scale.py
@@ -1,4 +1,3 @@
-from ttkbootstrap.types import Orientation
 from ttkbootstrap.style.builders.base import StyleBuilderBase
 from ttkbootstrap.style.element import ElementImage, Element
 from ttkbootstrap.style.utils import recolor_image
@@ -6,34 +5,22 @@ from ttkbootstrap.style.utils import recolor_image
 
 class ScaleStyleBuilder(StyleBuilderBase):
 
-    def __init__(self, color: str = "primary", orient: Orientation = "horizontal"):
-        super().__init__("TScale", color=color, orient=orient)
-
-    def color(self, value: str = None):
-        if value is None:
-            return self.options.get('color') or 'primary'
-        else:
-            self.options.update(color=value)
-            return self
-
-    def orient(self, value: Orientation = None):
-        if value is None:
-            return self.options.get('orient') or 'horizontal'
-        else:
-            self.options.update(orient=value)
-            return self
+    def __init__(self, **kwargs):
+        super().__init__("TScale", **kwargs)
+        self.options.setdefault('color', "primary")
+        self.options.setdefault("orient", "horizontal")
 
     def register_style(self):
         ttk_style = self.resolve_name()
         theme = self.theme
-        orient = self.orient().title()
+        orient = self.options.get('orient').title()
 
         # style colors
         background = theme.color(self.surface())
         foreground = theme.on_color(background)
         foreground_disabled = theme.disabled("text")
         track_color = theme.border(background)
-        handle_normal = theme.color(self.color())
+        handle_normal = theme.color(self.options.get('color'))
         handle_hover = theme.hover(handle_normal)
         handle_pressed = theme.active(handle_normal)
         handle_disabled = theme.disabled("text")
@@ -52,12 +39,14 @@ class ScaleStyleBuilder(StyleBuilderBase):
         side = "left" if orient == "Horizontal" else "top"
         padding = (8, 0, -8, 0) if orient == "Horizontal" else (0, 8, 0, -8)
 
-        self.create_element(ElementImage(f"{ttk_style}.{orient}.Scale.slider", handle_normal_img).state_specs([
-            ('disabled', handle_disabled_img),
-            ('focus', handle_focus_img),
-            ('pressed', handle_pressed_img),
-            ('hover', handle_hover_img)
-        ]))
+        self.create_element(
+            ElementImage(f"{ttk_style}.{orient}.Scale.slider", handle_normal_img).state_specs(
+                [
+                    ('disabled', handle_disabled_img),
+                    ('focus', handle_focus_img),
+                    ('pressed', handle_pressed_img),
+                    ('hover', handle_hover_img)
+                ]))
 
         self.create_element(
             ElementImage(f"{ttk_style}.{orient}.Scale.trough", track_normal_img, padding=padding, border=4).state_specs(
@@ -65,9 +54,11 @@ class ScaleStyleBuilder(StyleBuilderBase):
                     ('disabled', track_disabled_img)
                 ]))
 
-        self.style_layout(ttk_style, Element(f"{orient}.Scale.padding", sticky=sticky).children([
-            Element(f'{ttk_style}.{orient}.Scale.trough', sticky=sticky),
-            Element(f'{ttk_style}.{orient}.Scale.slider', sticky="", side=side)]))
+        self.style_layout(
+            ttk_style, Element(f"{orient}.Scale.padding", sticky=sticky).children(
+                [
+                    Element(f'{ttk_style}.{orient}.Scale.trough', sticky=sticky),
+                    Element(f'{ttk_style}.{orient}.Scale.slider', sticky="", side=side)]))
 
         self.configure(ttk_style, background=background, foreground=foreground)
         self.map(ttk_style, background=[], foreground=[('disabled', foreground_disabled)])
