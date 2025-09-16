8694e654cf55e65507e2284f4b27410e37d9ab7d
Israel Dryer <israel.dryer@gmail.com>
2025-09-16 15:25:16 -0400

    do not import private components

--- src/ttkbootstrap/layouts/__init__.py
+++ src/ttkbootstrap/layouts/__init__.py
@@ -1,16 +1,13 @@
-# ttkbootstrap/layouts/__init__.py
 from __future__ import annotations
 
 from importlib import import_module
 from typing import Any, TYPE_CHECKING
 
-__all__ = ["Grid", "Pack", "BaseLayout", "types"]
+__all__ = ["Grid", "Pack"]
 
 if TYPE_CHECKING:
     from .grid import Grid
     from .pack import Pack
-    from .base_layout import BaseLayout
-    from . import types  # module export
 
 
 def __getattr__(name: str) -> Any:
@@ -18,11 +15,6 @@ def __getattr__(name: str) -> Any:
         return getattr(import_module(".grid", __name__), "Grid")
     if name == "Pack":
         return getattr(import_module(".pack", __name__), "Pack")
-    if name == "BaseLayout":
-        return getattr(import_module(".base_layout", __name__), "BaseLayout")
-    if name == "types":
-        return import_module(".types", __name__)
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
 
 def __dir__():
