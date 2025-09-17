7c72ed5e6b6e292fd3816a358652175fd08e702b
Israel Dryer <israel.dryer@gmail.com>
2025-09-17 08:02:05 -0400

    update expander demo

--- tests/demo_expander.py
+++ tests/demo_expander.py
@@ -1,6 +1,6 @@
 from ttkbootstrap import App, Button, Checkbutton, Expander, Label, NumericEntry, Pack, TextEntry
 
-with App("Fieldset Demo", geometry="600x300", theme="dark") as app:
+with App("Fieldset Demo", geometry="600x300") as app:
     with Pack(padding=16, gap=16).layout(fill='both', expand=True):
         with Expander(title="My Stuff", collapsible=True).layout(fill='both') as ex:
             with ex.content:
@@ -11,11 +11,10 @@ with App("Fieldset Demo", geometry="600x300", theme="dark") as app:
                 Checkbutton()
                 Label("Custom Header Label")
             with ex.content:
-                with Pack(padding=1).layout(fill='both', expand=True):
-                    with Pack(fill_items='x', padding=16).layout(fill='both', expand=True):
-                        TextEntry(label="First Name")
-                        TextEntry(label="Last Name")
-                        NumericEntry(label="Age")
-                        TextEntry(label="Occupation")
+                with Pack(surface='background-1', fill_items='x', padding=16).layout(fill='both'):
+                    TextEntry(label="First Name")
+                    TextEntry(label="Last Name")
+                    NumericEntry(label="Age")
+                    TextEntry(label="Occupation")
 
 app.run()
