e6641688ae7c4393e7aef3bf87d1c0a433c8de2f
Israel Dryer <israel.dryer@gmail.com>
2025-09-25 23:49:46 -0400

    fix event handler

--- tests/demo_text_entry.py
+++ tests/demo_text_entry.py
@@ -1,6 +1,6 @@
 from ttkbootstrap import App, Grid, Button, Label, TextEntry, Event
 
-with App("Text Entry Demo", geometry="500x400") as app:
+with App("Text Entry Demo", geometry="600x400") as app:
     with Grid(padding=16, columns=2, gap=8, sticky_items="ew").layout(fill="x"):
         TextEntry(label="First Name", required=True).layout(columnspan=2).on_validated(lambda e: print(e))
         TextEntry("16228 Kelby Cove", label="Address").layout(columnspan=2).on_input(lambda x: print(x)).on_enter(
@@ -8,7 +8,7 @@ with App("Text Entry Demo", geometry="500x400") as app:
         TextEntry("July 15, 2025", label="Birthday", display_format="shortDate")
         occupation = TextEntry(label="Email", required=True).layout(columnspan=2)
         occupation.insert_addon(Label, text="@", position="left")
-        Button("Submit", icon="moon").on_invoke(lambda: app.theme.use("dark"))
+        Button("Submit", icon="moon").on_invoke().listen(lambda _: app.theme.use("dark"))
         Button("On Handler").on(Event.CLICK).filter(lambda e: e.x > 100).listen(lambda x: print(x))
         Button("Cancel", variant="outline", icon="sun").on_invoke().tap(lambda _: app.theme.use('light')).then_stop()
 app.run()
