c854f7368db120cdc14aa240ea3eb4fd7559fcdd
Israel Dryer <israel.dryer@gmail.com>
2025-09-29 22:13:26 -0400

    update event handling and signatures

--- tests/demo_radiobutton.py
+++ tests/demo_radiobutton.py
@@ -1,10 +1,11 @@
-from ttkbootstrap import App, Pack, Label, Radiobutton
+from ttkbootstrap import App, Label, Pack, Radiobutton
 
 with App() as app:
     with Pack(fill_items="x", gap=8, padding=16):
         Label("Radio Button Demo", font="heading-md")
         Radiobutton("One", 1, group="number")
         Radiobutton("Two", 2, color="danger", group="number")
-        Radiobutton("Three", 3, group="number").on_selected(lambda x: print(x)).on_deselected(
-            lambda x: print(x)).on_invoke(lambda x: print(x))
+        rb = Radiobutton("Three", 3, group="number")
+        rb.on_selected().listen(lambda x: print(x))
+        rb.on_deselected().listen(lambda x: print(x))
 app.run()
