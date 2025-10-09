9b41711376111f873c701ea907341e8c98f0c26b
Israel Dryer <israel.dryer@gmail.com>
2025-10-08 21:36:21 -0400

    Update docstring

--- src/ttkbootstrap/widgets/entry/variants/text.py
+++ src/ttkbootstrap/widgets/entry/variants/text.py
@@ -1,7 +1,7 @@
 from typing import Unpack
 
 from ttkbootstrap.widgets.entry.shared.entry_field import EntryField
-from ttkbootstrap.widgets.entry.types import TextEntryOptions
+from ttkbootstrap.widgets.entry.types import EntryFieldOptions
 
 
 class TextEntry(EntryField):
@@ -10,13 +10,34 @@ class TextEntry(EntryField):
 
     This widget extends `EntryField` for plain text input without any suffix or prefix addons.
     It is suitable for generic form fields such as name, email, or titles.
-
-    Parameters:
-        value: The initial text value.
-        label: Optional label text displayed above the input.
-        message: Optional helper or validation message below the input.
-        **kwargs: Additional keyword arguments passed to the EntryPart.
     """
 
-    def __init__(self, value: str = "", label: str = "", message: str = "", **kwargs: Unpack[TextEntryOptions]):
-        super().__init__(value, label, message, **kwargs)
+    def __init__(self, value: str = "", label: str = "", message: str = "", **kwargs: Unpack[EntryFieldOptions]):
+        """Initialize a TextEntry widget
+
+        Args:
+            value: The initial password value.
+            label: Label text displayed above the input.
+            message: Optional helper or validation message below the field.
+
+        Keyword Args:
+            allow_blank: If True, empty text commits to `None`
+            cursor: Mouse cursor when hovering.
+            display_format: Intl format spec for parsing/formatting (date/number, etc.).
+            export_selection: Whether selection is exported to the clipboard.
+            font: Font used to render text.
+            foreground: Text (foreground) color.
+            initial_focus: If true, this widget will receive focus on display.
+            justify: Text alignment within the entry.
+            kind: The input type, either "entry" or "spinbox".
+            label: The label text shown above the input field.
+            message: The caption or helper message shown below the input field.
+            padding: Inner padding around the content.
+            show: Mask character to display (e.g., '•').
+            take_focus: Whether the widget can receive focus.
+            text_variable: Variable bound to the entry text.
+            value: The initial value of the input field.
+            width: Widget width in characters.
+            x_scroll_command: Callback to connect a horizontal scrollbar.
+        """
+        super().__init__(value=value, label=label, message=message, **kwargs)
