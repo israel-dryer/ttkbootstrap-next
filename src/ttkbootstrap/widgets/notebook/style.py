cafdca546e85aef5223fa7f3ce00f4b32d50429d
Israel Dryer <israel.dryer@gmail.com>
2025-10-13 19:47:15 -0400

    refactor notebook style builder

--- src/ttkbootstrap/widgets/notebook/style.py
+++ src/ttkbootstrap/widgets/notebook/style.py
@@ -1,40 +1,44 @@
-from ttkbootstrap.style import StyleBuilderBase
+from ttkbootstrap.style import StyleManager
 
 
-class NotebookStyleBuilder(StyleBuilderBase):
-
+class NotebookStyleBuilder(StyleManager):
     def __init__(self, **kwargs):
         super().__init__("TNotebook", **kwargs)
+        self.options.set_defaults(variant="default")
 
-    def register_style(self):
-        self.build_default_style()
 
-    def build_default_style(self):
-        ttk_style = self.resolve_name()
-        surface_token = self.surface()
+@NotebookStyleBuilder.register_variant("default")
+def build_default_notebook_style(b: NotebookStyleBuilder):
+    ttk_style = b.resolve_ttk_name()
 
-        background = self.theme.color(surface_token)
-        foreground = self.theme.on_color(background)
-        border = self.theme.border(background)
-        self.configure(
-            ttk_style,
-            background=background,
-            bordercolor=border,
-            lightcolor=background,
-            darkcolor=background
-        )
+    background = b.color(b.surface_token)
+    foreground = b.on_color(background)
+    border = b.border(background)
+    b.style_configure(
+        ttk_style,
+        background=background,
+        bordercolor=border,
+        lightcolor=background,
+        darkcolor=background
+    )
 
-        self.configure(
-            f"{ttk_style}.Tab",
-            background=background,
-            foreground=foreground,
-            bordercolor=border,
-            lightcolor=background,
-            darkcolor=background,
-            focuscolor='')
+    b.style_configure(
+        f"{ttk_style}.Tab",
+        background=background,
+        foreground=foreground,
+        bordercolor=border,
+        lightcolor=background,
+        darkcolor=background,
+        focuscolor='')
 
-        self.map(
-            f"{ttk_style}.Tab",
-            foreground=[], background=[],
-            bordercolor=[('!selected', background)],
-            darkcolor=[], lightcolor=[])
+    b.style_map(
+        f"{ttk_style}.Tab",
+        foreground=[], background=[],
+        bordercolor=[('!selected', background)],
+        darkcolor=[], lightcolor=[])
+
+
+@NotebookStyleBuilder.register_variant("buttons")
+def build_button_tab_notebook_style(b: NotebookStyleBuilder):
+    # TODO implement bootstrap button style notebook
+    pass
