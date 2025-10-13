06ddee3024572937fdd872795ef64bb77ced56f1
Israel Dryer <israel.dryer@gmail.com>
2025-10-13 19:52:10 -0400

    refactor page stack style builder

--- src/ttkbootstrap/widgets/pagestack/style.py
+++ src/ttkbootstrap/widgets/pagestack/style.py
@@ -1,24 +1,22 @@
-from ttkbootstrap.style import StyleBuilderBase
+from ttkbootstrap.style import StyleManager
 
 
-class PageStackStyleBuilder(StyleBuilderBase):
-
+class PageStackStyleBuilder(StyleManager):
     def __init__(self, **kwargs):
         super().__init__("TNotebook", **kwargs)
+        self.options.set_defaults(variant="default")
 
-    def register_style(self):
-        self.build_default_style()
 
-    def build_default_style(self):
-        ttk_style = self.resolve_name()
-        surface_token = self.surface()
-        background = self.theme.color(surface_token)
-        self.configure(
-            ttk_style,
-            bordercolor=background,  # remove border
-            lightcolor=background,  # remove border
-            darkcolor=background,  # remove border
-            background=background,
-        )
-        # remove the tabs by creating an empty layout
-        self.style.ttk.layout(f'{ttk_style}.Tab', [])
+@PageStackStyleBuilder.register_variant("default")
+def build_default_page_stack_style(b: PageStackStyleBuilder):
+    ttk_style = b.resolve_ttk_name()
+    background = b.color(b.surface_token)
+    b.style_configure(
+        ttk_style,
+        bordercolor=background,  # remove border
+        lightcolor=background,  # remove border
+        darkcolor=background,  # remove border
+        background=background,
+    )
+    # remove the tabs by creating an empty layout
+    b.style.ttk.layout(f'{ttk_style}.Tab', [])
