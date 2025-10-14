81ffbcdd158283a1f0562816099c202125a30321
Israel Dryer <israel.dryer@gmail.com>
2025-10-13 23:34:22 -0400

    update demo with new api

--- tests/demo_virtual_listbox.py
+++ tests/demo_virtual_listbox.py
@@ -2,22 +2,22 @@ from ttkbootstrap import App, Pack, Button, VirtualList
 from ttkbootstrap.datasource import DataSource
 
 with App("Demo Virtual Listbox", geometry="500x500", theme="dark") as app:
-    records = [{"id": i, "text": f"Item {i}", "caption": "Caption", "icon": "house-fill"} for i in range(500)]
+    records = [{"id": i, "text": f"Item {i}", "caption": "Caption", "icon": "house-fill"} for i in range(50)]
 
     ds = DataSource(page_size=25)
     ds.set_data(records)
 
-    Button("Dark", on_invoke=lambda: app.theme.use("dark"))
-    Button("Light", on_invoke=lambda: app.theme.use("light"))
+    Button("Dark", command=lambda: app.theme.use("dark"))
+    Button("Light", command=lambda: app.theme.use("light"))
 
-    with Pack().layout(fill='both', expand=True):
+    with Pack().attach(fill='both', expand=True):
         VirtualList(
             items=ds,
-            selection_mode="multiple",
+            selection_mode="single",
             chevron_visible=True,
             selection_controls_visible=True,
             dragging_enabled=True,
             deleting_enabled=True,
-        ).layout(fill="both", expand=True)
+        ).attach(fill="both", expand=True)
 
 app.run()
