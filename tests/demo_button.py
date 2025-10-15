b51f2f27572825957bd70b3df2382d2b81db68a3
Israel Dryer <israel.dryer@gmail.com>
2025-10-15 18:23:07 -0400

    test new button state settings

--- tests/demo_button.py
+++ tests/demo_button.py
@@ -1,12 +1,16 @@
 from ttkbootstrap import App, Button, Pack
 
+icon = {
+    "name": "house",
+    "state": {"hover": "house-fill", "pressed": "apple"}
+}
+
 with App("Demo Button") as app:
     with Pack(padding=16).attach(fill="x"):
-        b = Button(icon="house-fill").attach()
-        Button("House", icon="house", command=lambda: b.configure(icon="airplane")).attach()
-        Button("House", icon="house-fill").attach().on_invoke().listen(lambda *_: b.configure(icon="house-fill"))
-        Button("House", icon="house-fill", color="warning").attach().on_invoke().listen(lambda e: print(e))
-        Button(icon="house", variant="outline", command=lambda e: print(e)).attach()
-        Button(icon="house", variant="ghost", color="dark", command=lambda: app.theme.use('light')).attach()
-        Button("House", icon="house", variant="outline", command=lambda: app.theme.use('dark')).attach()
+       b = Button("House", icon=icon).attach()
+       Button("House", icon="house-fill").attach().on_invoke().listen(lambda *_: b.configure(icon="house-fill"))
+       Button("House", icon="house-fill", color="warning").attach().on_invoke().listen(lambda e: print(e))
+       Button(icon="house", variant="outline", command=lambda e: print(e)).attach()
+       Button(icon="house", variant="ghost", color="dark", command=lambda: app.theme.use('light')).attach()
+       Button("House", icon="house", variant="outline", command=lambda: app.theme.use('dark')).attach()
 app.run()
