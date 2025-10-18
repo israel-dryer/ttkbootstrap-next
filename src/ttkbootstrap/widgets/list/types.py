f55c8643ab7cfde52ccac369455eeb9a7885a899
Israel Dryer <israel.dryer@gmail.com>
2025-10-18 07:07:45 -0400

    add types for list item options

--- src/ttkbootstrap/widgets/list/types.py
+++ src/ttkbootstrap/widgets/list/types.py
@@ -1,3 +1,16 @@
-from typing import Literal
+from tkinter import Widget
+from typing import Literal, TypedDict
 
 SelectBy = Literal['index', 'key']
+
+
+class ListItemOptions(TypedDict, total=False):
+    """Configuration provided by parent container"""
+    dragging_enabled: bool
+    deleting_enabled: bool
+    chevron_visible: bool
+    select_by_click: bool
+    selection_background: str
+    selection_mode: Literal['single', 'multiple', 'none']
+    selection_controls_visible: bool
+    parent: Widget
