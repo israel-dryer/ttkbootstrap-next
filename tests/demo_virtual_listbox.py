d29032918badf4947fe579eabc2b74498544408d
Israel Dryer <israel.dryer@gmail.com>
2025-10-18 21:46:10 -0400

    update demo for new api methods.

--- tests/demo_virtual_listbox.py
+++ tests/demo_virtual_listbox.py
@@ -1,10 +1,9 @@
-from ttkbootstrap import App, Pack, Button, VirtualList
+from ttkbootstrap import App, Button, Pack, VirtualList
 from ttkbootstrap.datasource import MemoryDataSource
 
 records = [{"id": i, "text": f"Item {i}", "caption": "Caption", "icon": "apple"} for i in range(100)]
 
 with App("Demo Virtual Listbox", geometry="500x500", theme="dark") as app:
-
     ds = MemoryDataSource(page_size=25)
     ds.set_data(records)
 
@@ -21,8 +20,10 @@ with App("Demo Virtual Listbox", geometry="500x500", theme="dark") as app:
             select_by_click=True,
         ).attach(fill="both", expand=True)
 
+        vl.on_item_deleting().cancel_when(lambda ev: ev.data['id'] == 105).listen(lambda e: print(e))
         vl.on_item_deleted().listen(lambda x: print(x))
-        vl.on_item_click().listen(lambda x: print(x))
+        # vl.on_item_click().listen(lambda x: print(x))
+        # vl.on_item_selected().listen(lambda x: print(x))
 
 app.run()
 
